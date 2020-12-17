ab794a8fbac1b0a2903cc4a410c24d483c9b7ae1
郭学胤 <7916402+guoxueyin@user.noreply.gitee.com>
2020-12-17 13:23:12 +0800

    Initial commit

---
+++ test/src/main/java/com/sztu/spring/Test.java
@@ -0,0 +1,11 @@
+package com.sztu.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("tx.xml");
+		Person bean =  classPathXmlApplicationContext.getBean(Person.class);
+		System.out.println(bean.getName());
+	}
+}
