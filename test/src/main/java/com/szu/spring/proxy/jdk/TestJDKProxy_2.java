77a817a93afc6f0959bf7790ce9a1de7f1a143aa
XueyinGuo <guoxueyin@163.com>
2021-02-28 11:49:37 +0800

    提交注释

---
+++ test/src/main/java/com/szu/spring/proxy/jdk/TestJDKProxy_2.java
@@ -0,0 +1,18 @@
+package com.szu.spring.proxy.jdk;
+
+
+public class TestJDKProxy_2 {
+
+	public static void main(String[] args) {
+
+		System.getProperties().put("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
+		System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+		Object proxy = new com.szu.spring.proxy.jdk.CalculatorProxy_2().getProxy(new com.szu.spring.proxy.jdk.MyCalculatorForJDKProxy());
+		if (proxy instanceof com.szu.spring.proxy.jdk.CalculatorForJDKProxy){
+			com.szu.spring.proxy.jdk.CalculatorForJDKProxy calculator = (com.szu.spring.proxy.jdk.CalculatorForJDKProxy) proxy;
+			int add = calculator.add(1, 1);
+			System.out.println(add);
+		}
+
+	}
+}
